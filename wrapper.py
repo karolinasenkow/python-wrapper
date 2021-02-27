077b649c230f00174291b141599d048e7339ea6e
Karolina Senkow <ksenkow@luc.edu>
2021-02-27 14:46:00 -0600

    improved q1 code

--- wrapper.py
+++ wrapper.py
@@ -1,38 +1,20 @@
 import os
 from Bio import Entrez
+from Bio import SeqIO
+file = open('input.txt').readlines()
 
 # Question 1
 # retrieve HCMV transcriptomes 2- and 6-days post-infection (dpi)
-
-# retrieve  2dpi (Donor 1)
-os.system('wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660030/SRR5660030.1')
-# convert to paired end fastq files
-os.system('fastq-dump -I --split-files SRR5660030.1')
-
-# retrieve 6dpi (Donor 1)
-os.system('wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660033/SRR5660033.1')
-# convert to paired end fastq files
-os.system('fastq-dump -I --split-files SRR5660033.1')
-
-# retrieve 2dpi (Donor 3)
-os.system('wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660044/SRR5660044.1')
-# convert to paired end fastq files
-os.system('fastq-dump -I --split-files SRR5660044.1')
-
-# retrieve 6dpi (Donor 3)
-os.system('wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5660045/SRR5660045.1')
-# convert to paired end fastq files
-os.system('fastq-dump -I --split-files SRR5660045.1')
+def fastq(file):
+	file = open(file).readlines()
+	for i in file:
+		# retrieve  2dpi (Donors 1, 3) & 6dpi (Donors 1, 3)
+		os.system('wget https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/' + i[:-3] + '/' + i)
+		# convert to paired end fastq files
+		os.system('fastq-dump -I --split-files ' + i)
 
 # Question 2
 # build a transcriptome index for HCMV (NCBI accession EF999921)
 # use biopython to retrieve and generate the appropriate input and then build the index with kallisto
 # you will need to extract the CDS features from the GenBank format
-
-# write output to outfile
-outfile = open('miniProject.log','w')
-
-# 
-Entrez.email = 'ksenkow@luc.edu'
-handle = Entrez.esearch(db="nucleotide", term=)
 
